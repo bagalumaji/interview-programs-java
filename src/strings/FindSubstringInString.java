29f0c48f81c70b9d83322ebcba469cf792468611
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-02-09 11:25:06 +0530

    sub strings in string

---
+++ src/strings/FindSubstringInString.java
@@ -0,0 +1,23 @@
+class FindSubstringInString {
+    public static String findSubstringInString(String []str) {
+            String mainSTring = str[0];
+            String []words = str[1].split(",");
+            String  firstPart="";
+            String secondPart="";
+            for(String word:words) {
+                int wordLength = word.length();
+                if(mainSTring.startsWith(word) && firstPart.length()<wordLength) {
+                    firstPart = word;
+                }else if(mainSTring.endsWith(word) && secondPart.length()<wordLength) {
+                    secondPart = word;
+                }
+            }
+            return firstPart+","+secondPart;
+    }
+
+    public static void main(String[] args) {
+        String[] str ={"baseball","base,all,a,ball"};
+        System.out.println(findSubstringInString(str));
+    }
+}
+
