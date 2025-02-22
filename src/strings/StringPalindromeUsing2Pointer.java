80d09f34f6451c6e863afde94cf58e23b8ba78c8
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-02-22 20:03:59 +0530

    string palindrome using 2 pointer

---
+++ src/strings/StringPalindromeUsing2Pointer.java
@@ -0,0 +1,21 @@
+package strings;
+
+public class StringPalindromeUsing2Pointer {
+    public static void main(String[] args) {
+        String str = "aba";
+        boolean flag=true;
+        int start=0;
+        int end = str.length()-1;
+        while(start<end){
+            if(str.charAt(start)!=str.charAt(end)){
+                flag=false;
+            }
+            start++;
+            end--;
+        }
+        if (flag)
+            System.out.println(str+" is palindrome");
+        else
+            System.out.println(str+" is not palindrome");
+    }
+}
