f96aac1c7c20e52b3201785cc8afbf97aec675c1
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-03-05 09:23:35 +0530

    updated with length method

--- src/strings/FirstNonRecurringCharacter.java
+++ src/strings/FirstNonRecurringCharacter.java
@@ -2,12 +2,12 @@ package strings;
 
 public class FirstNonRecurringCharacter {
     public static void main(String[] args) {
-        String str="java programming";
+        String str="java progrjamming";
         System.out.println("firstNonRecurringCharacter(str) = " + firstNonRecurringCharacter(str));
     }
     static char firstNonRecurringCharacter(String str){
         char ch=' ';
-        for (int i = 0; i < str.toCharArray().length; i++) {
+        for (int i = 0; i < str.length(); i++) {
             int l1 = str.length();
             ch= str.charAt(i);
             int l2 = str.replace(ch+"","").length();
