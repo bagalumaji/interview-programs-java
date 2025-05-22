0692b7dd579af7fb1d136ae57c894ec3f13d20c7
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-05-22 10:21:49 +0530

    Count The Consecutive Characters From String

---
+++ src/strings/CountConsecutiveCharacters.java
@@ -0,0 +1,20 @@
+package strings;
+
+public class CountConsecutiveCharacters {
+    public static void main(String[] args) {
+        System.out.println(getCountConsecutiveCharacters("yyyttyyyiio"));
+    }
+    static String getCountConsecutiveCharacters(String str){
+        int cnt=1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <=str.length(); i++) {
+            if(i<str.length() && str.charAt(i)==str.charAt(i-1)){
+                cnt++;
+            }else {
+                sb.append(str.charAt(i-1)).append(":").append(cnt).append(" ");
+                cnt=1;
+            }
+        }
+        return sb.toString();
+    }
+}
