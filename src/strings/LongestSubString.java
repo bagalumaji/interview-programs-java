bbf9eb38f014685f81ec41e719db487dfaf1684f
umajibagal <bagalumaji@gmail.com>
2025-05-23 10:05:34 +0530

    updated

--- src/strings/LongestSubString.java
+++ src/strings/LongestSubString.java
@@ -4,28 +4,32 @@ import java.util.HashSet;
 
 public class LongestSubString {
     public static void main(String[] args) {
-        String  str = "abcaebafd";
+        String str = "bcadeffre";
         System.out.println(getLongestSubString(str));
     }
-    static int getLongestSubString(String str){
-        int start=0;
-        int end=0;
-        int max=0;
+
+    static int getLongestSubString(String str) {
+        int start = 0;
+        int end = 0;
+        int max = 0;
+        String result = "";
         HashSet<Character> characters = new HashSet<>();
 
-        while(end!=str.length()){
-            if(!characters.contains(str.charAt(end))){
+        while (end != str.length()) {
+            if (!characters.contains(str.charAt(end))) {
                 characters.add(str.charAt(end));
                 end++;
-                max = Math.max(characters.size(),max);
-            }
-            else {
+                if (max < characters.size()) {
+                    max = characters.size();
+                    result = str.substring(start, end);
+                }
+            } else {
                 characters.remove(str.charAt(start));
                 start++;
             }
         }
-        System.out.println(str.substring(start, end));
-        return max;
+        System.out.println(result);
+        return result.length();
 
     }
 }
