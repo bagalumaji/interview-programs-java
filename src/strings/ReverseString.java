50e874d160e60c57e2e10993d04a4ea35ce078a1
umajibagal <bagalumaji@gmail.com>
2025-08-11 21:05:00 +0530

    removed print

--- src/strings/ReverseString.java
+++ src/strings/ReverseString.java
@@ -10,7 +10,6 @@ public class ReverseString {
         for (int i = str.length()-1; i>=0 ; i--) {
             sb.append(str.charAt(i));
         }
-        System.out.println("sb = " + sb);
         return sb.toString();
     }
 }
